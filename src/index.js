6db13bcca97df6ead494625d508017ac4b5c027b
Joey Grisafe <jgrisafe@gmail.com>
2021-03-05 23:32:34 -0800

    whoops forgot to pass options

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import camelcaseKeys from './camelcaseKeys';
 
 const defaultOptions = { global: true };
 
-const checkCamelCaseKey = (action) => {
+const checkCamelCaseKey = (action, options) => {
   if (typeof action === 'object') {
     return (options.global || !!action.camelCase || !!action.meta.arg.camelCase)
       && action.camelCase !== false
@@ -10,7 +10,7 @@ const checkCamelCaseKey = (action) => {
 }
 
 const camelMiddleware = (options = defaultOptions) => store => next => action => {
-  const shouldCamelCaseKeys =  checkCamelCaseKey(action);
+  const shouldCamelCaseKeys =  checkCamelCaseKey(action, options);
 
   if (typeof action === 'object' && shouldCamelCaseKeys) {
     const newAction = camelcaseKeys(action);
